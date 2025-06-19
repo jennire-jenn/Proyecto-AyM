64ac1ae575e11a309fa7d1da940aa1509375f3ff
jennire-jenn <jennire.martinez@davinci.edu.ar>
2025-06-19 09:23:57 -0300

    bloques nivel 1

--- componentes/bloque.py
+++ componentes/bloque.py
@@ -2,10 +2,9 @@ import pygame
 from pygame.math import Vector2
 
 class Bloque:
-    def __init__(self, x, y, color):
+    def __init__(self, x, y, color,img):
         self.pos = Vector2(x, y)
-        self.color = color
-        self.img = pygame.image.load('img/bloque.png').convert_alpha()
+        self.img = img
 
     def dibujar(self, screen, cell_size):
         rect = pygame.Rect(self.pos.x * cell_size, self.pos.y * cell_size, cell_size, cell_size)
